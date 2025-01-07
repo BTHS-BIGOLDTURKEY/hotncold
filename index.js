27186693c378fe4a6ac34f95c63abbeab78f33b9
Zakariaa Lotfi <144473402+BTHS-BIGOLDTURKEY@users.noreply.github.com>
2025-01-07 11:00:37 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,84 @@
+/* === Imports === */
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+/* === Firebase Setup === */
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBY-cMFu0fxSyfOuX86rmSli6K-I0qRPD4",
+    authDomain: "hot-and-cold-fa57e.firebaseapp.com",
+    projectId: "hot-and-cold-fa57e",
+    storageBucket: "hot-and-cold-fa57e.firebasestorage.app",
+    messagingSenderId: "899917774474",
+    appId: "1:899917774474:web:4987e1e8e2af70bb51b773"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+console.log(app.options.projectId);
+
+/* === UI === */
+
+/* == UI - Elements == */
+
+const viewLoggedOut = document.getElementById("logged-out-view")
+const viewLoggedIn = document.getElementById("logged-in-view")
+
+const signInWithGoogleButtonEl = document.getElementById("sign-in-with-google-btn")
+
+const emailInputEl = document.getElementById("email-input")
+const passwordInputEl = document.getElementById("password-input")
+
+const signInButtonEl = document.getElementById("sign-in-btn")
+const createAccountButtonEl = document.getElementById("create-account-btn")
+
+/* == UI - Event Listeners == */
+
+signInWithGoogleButtonEl.addEventListener("click", authSignInWithGoogle)
+
+signInButtonEl.addEventListener("click", authSignInWithEmail)
+createAccountButtonEl.addEventListener("click", authCreateAccountWithEmail)
+
+/* === Main Code === */
+
+showLoggedOutView()
+
+/* === Functions === */
+
+/* = Functions - Firebase - Authentication = */
+
+function authSignInWithGoogle() {
+    console.log("Sign in with Google")
+}
+
+function authSignInWithEmail() {
+    console.log("Sign in with email and password")
+}
+
+function authCreateAccountWithEmail() {
+    console.log("Sign up with email and password")
+}
+
+/* == Functions - UI Functions == */
+
+function showLoggedOutView() {
+    hideElement(viewLoggedIn)
+    showElement(viewLoggedOut)
+}
+
+function showLoggedInView() {
+    hideElement(viewLoggedOut)
+    showElement(viewLoggedIn)
+}
+
+function showElement(element) {
+    element.style.display = "flex"
+}
+
+function hideElement(element) {
+    element.style.display = "none"
+}
+
+//credit: coursera
